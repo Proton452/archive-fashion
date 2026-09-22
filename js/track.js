432eca85a764434104caa0b674c2d3297ae9595d
Mikey <dzoox8@gmail.com>
2026-09-22 16:55:41 +0200

    Remove tracking widget (17track deep-link did not work)

--- js/track.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('trackForm')?.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const input = document.getElementById('trackInput');
-  const num = input.value.trim();
-  if (!num) return;
-  window.open('https://t.17track.net/en#nums=' + encodeURIComponent(num), '_blank', 'noopener');
-});
